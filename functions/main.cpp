c06debce66eff0658ed3263db01d95472e39913f
andrescabana86 <andrescabana86@gmail.com>
2022-03-12 16:19:03 -0300

    C++ Training with functions changes: * basic function training

--- functions/main.cpp
+++ functions/main.cpp
@@ -1,3 +1,25 @@
+#include <iostream>
+/*
+ * Functions has a name, a return type and definition body
+ * We don't necessary need parameters, it's optional, when it is present, it should has a type definition
+ * We do need a return type, but if we don't return a type we can define it as 'void'
+ * The primary function of a function is to prevent duplication
+ */
+
+int MultiplyTwoNumbers(int a, int b)
+{
+    return a * b;
+}
+
+void MultiplyAndLog(int a, int b)
+{
+    int result = MultiplyTwoNumbers(a,b);
+    std::cout << "Result: " << result << std::endl;
+}
+
 int main() {
+    MultiplyAndLog(4,2);
+    MultiplyAndLog(43,22);
+    MultiplyAndLog(142,32);
     return 0;
 }
