100846ca646df5f8821b90ade86d948e1aef0ae5
andrescabana86 <andrescabana86@gmail.com>
2022-03-12 15:31:46 -0300

    C++ Training with variables changes: * separate by projects * create a variables project * console-io improvements

---
+++ console-io/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "This is a title" << "\n";
+    std::cout << "This is below title" << std::endl;
+    std::cout << "next line" << "\n";
+    std::cout << "Calculated integer number " << (2+2) << "\n";
+    std::cout << "Calculated float number " << (2.1+2.3) << "\n";
+    std::cout << "end!" << std::endl;
+    return 0;
+}
---
+++ variables/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+int main() {
+    int integer = 8; // 4 bytes large (32bits total) range -2b < 0 < 2b
+    std::cout << "signed integer: " << integer << std::endl;
+    // unsigned numbers are only positive numbers, using 1 bit more by removing the sign
+    unsigned unsigned_integer = 8; // 4 bytes large (32bits total) range 0 < 4b
+    std::cout << "unsigned number (always positive): " << unsigned_integer << std::endl;
+
+    // other types of numbers, all values primitives
+    /*
+     * char 1 byte of data
+     * short 2 bytes of data
+     * int 4 bytes of data
+     * long usually 4 bytes of data, depending on the compiler
+     * long long 8 bytes of data
+     * float 4 bytes of data
+     * double 8 bytes of data
+     * bool 1 byte
+     */
+
+    // type char is numeric, if you save a number or a letter it will always be interpreted as a letter
+    char letter = 'A';
+    std::cout << "this is a letter inside a char: " << letter << std::endl;
+    letter = 65;
+    std::cout << "this is a number inside a char: " << letter << std::endl;
+
+    /*
+     * float a double are decimals representations
+     * the way we decide which one to use is
+     * to append an 'f' at the end of the number
+     */
+    float float_number = 5.5; // this is a double
+    std::cout << "this is a float compiled as double: " << float_number << std::endl;
+    float real_float_number = 5.5f; // this is a float
+    std::cout << "this is a float compiled as float: " << real_float_number << std::endl;
+    double double_number = 5.5; // this is also a double
+    std::cout << "this is a double: " << double_number << std::endl;
+
+    // bool uses 1 byte, meaning 8 bits
+    // we could use 1 bit by doing optimizations
+    // in general, computer cannot manage bits so
+    // bool will always ocupy 1 byte
+    bool bool_value = true;
+    std::cout << "this is a bool: " << bool_value << std::endl;
+
+    return 0;
+}
