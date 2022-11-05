5f2739d5d1490828318faa73f55b79dc3c6518b4
andrescabana86 <andrescabana86@gmail.com>
2022-11-05 19:36:20 -0300

    C++ Training with parallel and concurrency changes: * created a inverted array exercise with pointers

--- templates/main.cpp
+++ templates/main.cpp
@@ -19,6 +19,19 @@ void print(const int (&array)[size][size])
 }
 
 template <size_t size>
+void printPtr(int **array)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        std::cout << i << ": ";
+        for (size_t j = 0; j < size; ++j) {
+            std::cout << array[i][j] << '\t';
+        }
+        std::cout << std::endl;
+    }
+}
+
+template <size_t size>
 void transpose(const int (&array)[size][size], int (&result)[size][size])
 {
     for (size_t i = 0; i < size; ++i)
@@ -29,7 +42,19 @@ void transpose(const int (&array)[size][size], int (&result)[size][size])
     }
 }
 
+template <size_t size>
+void transposePtr(const int (&array)[size][size], int **result)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        for (size_t j = 0; j < size; ++j) {
+            result[j][i] = array[i][j];
+        }
+    }
+}
+
 int main() {
+    // using a fixed size two dim array
     const int size = 3;
     int twoDimArr[size][size] = {
         1,2,3,4,
@@ -41,6 +66,17 @@ int main() {
     print<size>(twoDimArr);
     transpose(twoDimArr, result);
     print<size>(result);
+
+    // using two dim pointer array
+    int ** ptrResult = new int*[size];
+    for (size_t i = 0; i < size; ++i)
+    {
+        ptrResult[i] = new int[size];
+    }
+
+    transposePtr<size>(twoDimArr, ptrResult);
+    printPtr<size>(ptrResult);
+
 
     return 0;
 }
