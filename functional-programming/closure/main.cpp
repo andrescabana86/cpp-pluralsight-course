d41745b30e20632a797addabfe8369aa1a97c226
andrescabana86 <andrescabana86@gmail.com>
2023-01-29 23:46:50 -0300

    C++ Training: * Lambda Closure usage, with parameters

--- functional-programming/closure/main.cpp
+++ functional-programming/closure/main.cpp
@@ -10,10 +10,22 @@ auto closureFunction = []() {
     };
 };
 
+auto closureWithInitial = [](const int initial, const int add) {
+    int increment = initial;
+    return [increment,add]() mutable {
+        return increment += add;
+    };
+};
+
 int main() {
     auto counter = closureFunction();
     cout << "increment result is: " << counter() << endl;
     cout << "increment result is: " << counter() << endl;
     cout << "increment result is: " << counter() << endl;
+
+    auto adder = closureWithInitial(0, 5);
+    cout << "increment result is: " << adder() << endl;
+    cout << "increment result is: " << adder() << endl;
+    cout << "increment result is: " << adder() << endl;
     return 0;
 }
