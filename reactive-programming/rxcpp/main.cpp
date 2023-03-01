056959e88e188a356f446e754cee9c74d5333450
andrescabana86 <andrescabana86@gmail.com>
2023-03-01 13:18:58 -0300

    C++ Training: RxCpp library * adding to ToDo

---
+++ reactive-programming/rxcpp/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by andrescabana86 on 1/3/2023.
+//
+/*
+ * ReactiveX extensions implements observer pattern
+ * components:
+ * - observables: composable streams of events
+ * - observers: those interested on observables events
+ * - operators: manipulate observables
+ * - schedulers: execute actions related to operators
+ * The RxCpp Library:
+ * - it's a header only, c++ implementation
+ * - can convert from STL containers to observables
+ * - supports either a chaining or a pipe interface
+ */
+
+
+
