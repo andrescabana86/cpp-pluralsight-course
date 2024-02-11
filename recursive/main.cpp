f8e9a1d3e17dfa1c5eaa26916a834cf8c3f58757
andrescabana86 <andrescabana86@gmail.com>
2024-02-11 12:48:40 -0300

    C++ Training: add recursive project

---
+++ recursive/main.cpp
@@ -0,0 +1,11 @@
+/**
+ * Recursive fn() are functions that reference itself to
+ * split the problem and solve in multiple steps using the
+ * same function by concatenating results of nested calls
+ */
+
+
+
+int main() {
+    return 0;
+}
